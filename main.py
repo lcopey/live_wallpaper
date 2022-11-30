56b45cd47d1f55a9a455a1fe24af4bce6af07bd8
lcopey <laurent.copey@gmail.com>
2022-11-30 22:12:45 +0100

    added logging

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from live_wallpaper import set_latest_as_wallpaper
 from rocketry import Rocketry
 from rocketry.conds import every
+from structlog import getLogger
+from datetime import datetime
 
 app = Rocketry()
+logger = getLogger()
 
 
-@app.task(every("15 minutes"))
+# @app.task(every("15 minutes"))
+# def task():
+#     """Set the wallpaper live."""
+
+@app.task('every 15 minutes')
 def task():
-    """Set the wallpaper live."""
     set_latest_as_wallpaper('meteosat-11')
+    now = datetime.now().strftime('%H:%M:%S')
+    logger.msg(time=now, msg='wallpaper changed')
 
 
 if __name__ == '__main__':
